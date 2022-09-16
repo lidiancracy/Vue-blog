542a6ff7cd4b1403861d915de9b57ec155331df9
lidian <837997288@qq.com>
2022-09-16 20:38:36 +0800

    日志

--- sg-blog/src/test/java/com/sangeng/controller/test1.java
+++ sg-blog/src/test/java/com/sangeng/controller/test1.java
@@ -1,6 +1,9 @@
 package com.sangeng.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,6 +16,7 @@ import org.springframework.mail.javamail.JavaMailSender;
  * @Description 日志功能以及发送邮件功能
  * @Date 2022/9/16 17:27
  */
+@Slf4j
 @SpringBootTest
 class test1 {
     @Autowired
@@ -21,12 +25,17 @@ class test1 {
     private String from;
 
 
+
     @Test
-    public void test1(){
+
+    public void test1() {
         //创建简单邮件消息
         SimpleMailMessage message = new SimpleMailMessage();
         //谁发的
         message.setFrom(from);
+        log.info("asd");
+        log.warn("warn");
+        log.error("eoor");
         //谁要接收
         message.setTo("1960870058@qq.com");
         //邮件标题
